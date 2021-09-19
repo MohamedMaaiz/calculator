08d1136a0b12276d3fcd05bc280cc3de2338789c
Mohamed Maaiz <mohammadh3g@gmail.com>
2021-09-20 00:03:09 +0500

    made the files and added buttons to html and some basic math functions to js

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+let add = (num1, num2) => num1 + num2;
+let subtract = (num1, num2) => num1 - num2;
+let multiply = (num1, num2) => num1 * num2;
+let divide = (num1, num2) => num1 / num2;
+
+function calculate(num1, num2){
+    return add(num1, num2);
+};
+
+console.log(calculate(2,3))
