e01aac0694faece281d7e20395026c0b1127d6cf
Mohamed Maaiz <mohammadh3g@gmail.com>
2021-09-23 00:09:28 +0500

    fixed the arrow with display and added decimal point to numbers

--- script.js
+++ script.js
@@ -10,109 +10,91 @@ equalsBTN.onclick = () => calculate();
 clearBTN.onclick = () => clearScreen();
 
 //current values
-let currentNumber = 0; //the number on the screen
-let firstNumber = 0; //first number to be used
+let currentNumber = ''; //the number on the screen
+let firstNumber = ''; //first number to be used
 let firstNumExitst = false;
-let secondNumber = 0; //secont number to be used
-let result = 0; //store the result
+let secondNumber = ''; //secont number to be used
+let result = ''; //store the result
 let resultExist = false; //to check for result 
 let usedOperator = ''; //user selected operator
 
-//calculations
-let add = (num1, num2) => num1 + num2;
-let subtract = (num1, num2) => num1 - num2;
-let multiply = (num1, num2) => num1 * num2;
-let divide = (num1, num2) => num1 / num2;
-
 numberBTN.forEach(button => {
-    button.addEventListener('click', () => {
-        updateDisplayValue(button.innerHTML);
-    })
-});
+    button.addEventListener('click', () => {numberUpdate(button.textContent)});
+})
 
 operatorBTN.forEach(button => {
-    button.addEventListener('click', () => {
-        usedOperator = button.innerHTML; 
-        resultExist == true ? firstNumber = result : firstNumber = currentNumber;
-        updateDisplayValue(` ${button.innerHTML} `);
-        firstNumExitst = true;
+    button.addEventListener('click', () => {operatorUpdate(button.textContent)});
+})
 
-    
-    console.log('first +'+firstNumber)
-
-    })
-});
-
-//update user display value with button press
-function updateDisplayValue(buttonPressed) {
-
-    if (resultExist == true) {
-        currentNumber = result;
-        resultExist = false;
-    } else {
-        currentNumber = numberDisplay.textContent;
-    }
-
-    console.log('current '+currentNumber)
-    
-    currentNumber = numberDisplay.textContent = currentNumber + buttonPressed;
-    
+function numberUpdate(number) {
+    currentNumber += number;
+    updateDisplayValue(number);
     if (firstNumExitst == true) calculate();
 }
 
+function operatorUpdate(operator) {
+    usedOperator = operator;
+    resultExist == true ? firstNumber = result : firstNumber = currentNumber;
+    firstNumExitst = true;
+    let type = 'symbol';
+    updateDisplayValue(` ${operator} `,type);
+}
+
 function calculate() {
-    
     getSecondNumber()
     operatorSelection();
-    resultExist = true;
-    console.log('first '+firstNumber)
+    resultExist = true
 }
 
 function getSecondNumber() {
     let splited = currentNumber.toString().split(' ');
-    console.log(splited)
     secondNumber = splited.pop();
-    
-    console.log('second '+secondNumber)
-    if (secondNumber === '') {
-        
-        return console.log('no second')
-    }
 }
 
 function displayResult(solution) {
+    solution = Math.round(solution * 1000) / 1000;
     resultDisplay.textContent = solution;
     result = solution;
 }
 
 function clearScreen() {
-    currentNumber = 0;
-    firstNumber = 0;
-    secondNumber = 0;
+    currentNumber = '';
+    firstNumber = '';
+    firstNumExitst = false
+    secondNumber = '';
     usedOperator = '';
-    result = 0;
+    result = '';
     resultExist = false;
     numberDisplay.textContent = '';
     resultDisplay.textContent = '';
 }
 
-function operatorSelection() {
+//update user display value with button press
+function updateDisplayValue(buttonPressed,type) {
+    let lastValue = numberDisplay.textContent;
+    if (resultExist == true && type === 'symbol') lastValue = result;
+    currentNumber = lastValue + buttonPressed
+    numberDisplay.textContent = currentNumber
+}
 
+function operatorSelection() {
+let a = Number(firstNumber),b = Number(secondNumber);
     switch (usedOperator) {
         case '+':
-            displayResult(add(parseInt(firstNumber), parseInt(secondNumber)));
-            break;
+            return displayResult(add(a, b));
         case '-':
-            displayResult(subtract(parseInt(firstNumber), parseInt(secondNumber)));
-            break;
+            return displayResult(subtract(a, b));
         case 'x':
-            displayResult(multiply(parseInt(firstNumber), parseInt(secondNumber)));
-            break;
+            return displayResult(multiply(a, b));
         case '÷':
-            displayResult(divide(parseInt(firstNumber), parseInt(secondNumber)));
-            break;
+            return displayResult(divide(a, b));
         default:
             break;
     }
-    firstNumExitst = false;
 };
+
+//calculations
+let add = (a, b) => a + b;
+let subtract = (a, b) => a - b;
+let multiply = (a, b) => a * b;
+let divide = (a, b) => b === 0 ? false : a / b;
